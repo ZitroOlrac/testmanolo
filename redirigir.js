5b37649d1aaba5d98cf129d0854a1732edc6a38d
Carlo Fabian Prado Ortiz <64349589+ZitroOlrac@users.noreply.github.com>
2024-06-11 19:39:17 -0500

    Update redirigir.js

--- redirigir.js
+++ redirigir.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function redirigirWhatsApp() {
-    var telefono = "+51907947044";
+    var telefono = "+51913209611";
     var mensaje = "Hola, me gustaría solicitar una cotización, ¿podrías proporcionarme más información al respecto?";
     var url = "https://api.whatsapp.com/send?phone=" + telefono + "&text=" + encodeURIComponent(mensaje);
     window.open(url, '_blank');
